e9e42bad9bcdd1b668b66b5316dccc6d7b99e4b2
Markus Ast <m@rkusa.st>
2018-12-05 16:49:38 +0100

    day 2 part 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -27,7 +27,45 @@ fn part1() -> Result<i32, io::Error> {
     Ok(multiples.values().product())
 }
 
+fn part2() -> Result<Option<String>, io::Error> {
+    struct ID(String);
+
+    impl PartialEq for ID {
+        fn eq(&self, other: &ID) -> bool {
+            let dist: usize = self
+                .0
+                .chars()
+                .zip(other.0.chars())
+                .map(|(l, r)| if l != r { 1 } else { 0 })
+                .sum();
+            dist == 1
+        }
+    }
+
+    let mut ids: Vec<ID> = BufReader::new(File::open("input.txt")?)
+        .lines()
+        .filter_map(Result::ok)
+        .map(ID)
+        .collect();
+
+    while let Some(id) = ids.pop() {
+        for other in &ids {
+            if &id == other {
+                let common =
+                    id.0.chars()
+                        .zip(other.0.chars())
+                        .filter_map(|(l, r)| if l == r { Some(l) } else { None })
+                        .collect::<String>();
+                return Ok(Some(common));
+            }
+        }
+    }
+
+    Ok(None)
+}
+
 fn main() -> Result<(), io::Error> {
     println!("Part 1: {}", part1()?);
+    println!("Part 2: {}", part2()?.unwrap_or_default());
     Ok(())
 }
